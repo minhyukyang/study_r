6237329c576373fce57417450c9ddf898ea86f3b
minhyuk.yang <minhyuk.yang@gmail.com>
2019-04-24 17:58:38 +0900

    add files.

---
+++ TextMining/Sentiment_Analysis_with_googleLanguageR.R
@@ -0,0 +1,7 @@
+# Goal : Novel Analysis
+# Reference : https://junhewk.github.io/text/2017/10/05/googlelanguageR-and-novel-analysis/
+
+
+# 0. Set the Environment --------------------------------------------------
+
+# we need a google cloud account... :(
