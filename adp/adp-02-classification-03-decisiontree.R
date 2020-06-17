a30a7539f17162acbbf4f4dbf1b658b726affdb8
minhyuk.yang <minhyuk.yang@gmail.com>
2020-06-17 20:37:03 +0900

    mofidy file

--- adp/adp-02-classification-03-decisiontree.R
+++ adp/adp-02-classification-03-decisiontree.R
@@ -1,142 +1,89 @@
 # 3. 의사결정나무 모형 
 
 set.seed(2020)
-
 data(iris)
 
-ind <- sample(2, nrow(iris), replace=TRUE, prob=c(0.7, 0.3))
+library(caret)
 
-trainData <- iris[ind==1,]
-testData <- iris[ind==2,]
+idx <- createDataPartition(y=iris$Species, p=0.7, list=FALSE)
+# ind <- sample(2, nrow(iris), replace=TRUE, prob=c(0.7, 0.3))
 
-# 1. rpart ----------------------------------------------------------------
+trainData <- iris[idx,]
+testData <- iris[-idx,]
+
+
+# 1. tree -----------------------------------------------------------------
+
+library(tree)
+
+myFormula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
+
+# Model
+iris_tree <- tree(myFormula, data=trainData)
+plot(iris_tree)
+text(iris_tree)
+
+# Pruning
+cv.trees <- cv.tree(iris_tree, FUN=prune.misclass)
+plot(cv.trees) # 분산이 가장 낮은 클래스 선택
+
+prune.trees <- prune.misclass(iris_tree, best=3)
+plot(prune.trees)
+text(prune.trees)
+
+# Prediction & Evaluation
+library(e1071)
+
+treepred <- predict(prune.trees, testData, type='class')
+confusionMatrix(treepred, testData$Species) # 0.9333
+
+
+# 2. rpart ----------------------------------------------------------------
 
 library(rpart)
 
 myFormula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
 
-bodyfat_rpart <- rpart(myFormula, data = iris, control = rpart.control(minsplit = 10))
+# Model
+iris_rpart <- rpart(myFormula, data = trainData, method="class")
+# iris_rpart <- rpart(myFormula, data = iris, control = rpart.control(minsplit = 10))
+plot(iris_rpart)
+text(iris_rpart)
 
-attributes(bodyfat_rpart)
-# $names
-# [1] "frame"               "where"               "call"                "terms"               "cptable"             "method"             
-# [7] "parms"               "control"             "functions"           "numresp"             "splits"              "variable.importance"
-# [13] "y"                   "ordered"            
-# 
-# $xlevels
-# named list()
-# 
-# $ylevels
-# [1] "setosa"     "versicolor" "virginica" 
-# 
-# $class
-# [1] "rpart"
+printcp(iris_rpart) # xerror이 가장 낮은 split 개수 선택
+#        CP nsplit rel error   xerror     xstd
+# 1 0.50000      0  1.000000 1.142857 0.062348
+# 2 0.45714      1  0.500000 0.742857 0.073189
+# 3 0.01000      2  0.042857 0.085714 0.033978
 
-print(bodyfat_rpart)
-# n= 150 
-# 
-# node), split, n, loss, yval, (yprob)
-# * denotes terminal node
-# 
-# 1) root 150 100 setosa (0.33333333 0.33333333 0.33333333)  
-#   2) Petal.Length< 2.45 50   0 setosa (1.00000000 0.00000000 0.00000000) *
-#   3) Petal.Length>=2.45 100  50 versicolor (0.00000000 0.50000000 0.50000000)  
-#     6) Petal.Width< 1.75 54   5 versicolor (0.00000000 0.90740741 0.09259259)  
-#       12) Petal.Length< 4.95 48   1 versicolor (0.00000000 0.97916667 0.02083333) *
-#       13) Petal.Length>=4.95 6   2 virginica (0.00000000 0.33333333 0.66666667) *
-#     7) Petal.Width>=1.75 46   1 virginica (0.00000000 0.02173913 0.97826087) *
+plotcp(iris_rpart)
 
-plot(bodyfat_rpart)
-text(bodyfat_rpart, use.n=TRUE)
+# Pruning
+ptree <- prune(iris_rpart, cp=iris_rpart$cptable[which.min(iris_rpart$cptable[,"xerror"]),])
+plot(ptree)
+text(ptree, use.n=TRUE)
 
-bodyfat_rpart$cptable
-#    CP nsplit rel error xerror       xstd
-# 1 0.50      0      1.00   1.14 0.05230679
-# 2 0.44      1      0.50   0.59 0.05982753
-# 3 0.02      2      0.06   0.11 0.03192700
-# 4 0.01      3      0.04   0.11 0.03192700
+# Prediction & Evaluation
+rpartpred <- predict(ptree, testData, type="class")
+confusionMatrix(rpartpred, testData$Species) # 0.9333         
 
-opt <- which.min(bodyfat_rpart$cptable[, "xerror"])
-cp <- bodyfat_rpart$cptable[opt, "CP"]
-prune.c <- prune(bodyfat_rpart, cp=cp)
-# n= 150 
-# 
-# node), split, n, loss, yval, (yprob)
-# * denotes terminal node
-# 
-# 1) root 150 100 setosa (0.33333333 0.33333333 0.33333333)  
-# 2) Petal.Length< 2.45 50   0 setosa (1.00000000 0.00000000 0.00000000) *
-#   3) Petal.Length>=2.45 100  50 versicolor (0.00000000 0.50000000 0.50000000)  
-# 6) Petal.Width< 1.75 54   5 versicolor (0.00000000 0.90740741 0.09259259) *
-#   7) Petal.Width>=1.75 46   1 virginica (0.00000000 0.02173913 0.97826087) *
-
-plot(prune.c)
-text(prune.c, use.n=T)
 # `$cptable`은 트리의 크기에 따른 비용-복잡도 모수를 제공하며, 교차 타당성 오차를 함께 제공함
 # 이 값들은 `prune()` 또는 `rpart.control()` 함수에서 가지치기와 트리의 최대 크기를 조절하기 위한 옵션으로 사용됨
 
-plotcp(bodyfat_rpart) # cp값 출력
 
+# 3. party ----------------------------------------------------------------
 
-# 2. party ----------------------------------------------------------------
-
-# install.packages("party")
 library(party)
-
-data(iris)
-summary(iris)
-#  Sepal.Length    Sepal.Width     Petal.Length    Petal.Width          Species  
-# Min.   :4.300   Min.   :2.000   Min.   :1.000   Min.   :0.100   setosa    :50  
-# 1st Qu.:5.100   1st Qu.:2.800   1st Qu.:1.600   1st Qu.:0.300   versicolor:50  
-# Median :5.800   Median :3.000   Median :4.350   Median :1.300   virginica :50  
-# Mean   :5.843   Mean   :3.057   Mean   :3.758   Mean   :1.199                  
-# 3rd Qu.:6.400   3rd Qu.:3.300   3rd Qu.:5.100   3rd Qu.:1.800                  
-# Max.   :7.900   Max.   :4.400   Max.   :6.900   Max.   :2.500 
-
-ind <- sample(2, nrow(iris), replace=TRUE, prob=c(0.7, 0.3))
-
-trainData <- iris[ind==1,]
-testData <- iris[ind==2,]
 
 myFormula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
 
+# Model
 iris_ctree <- ctree(myFormula, data=trainData)
-
-table(predict(iris_ctree), trainData$Species)
-#            setosa versicolor virginica
-# setosa         33          0         0
-# versicolor      0         36         1
-# virginica       0          4        34
-
-print(iris_ctree)
-#	 Conditional inference tree with 4 terminal nodes
-# 
-# Response:  Species 
-# Inputs:  Sepal.Length, Sepal.Width, Petal.Length, Petal.Width 
-# Number of observations:  108 
-# 
-# 1) Petal.Length <= 1.9; criterion = 1, statistic = 100.5
-#   2)*  weights = 33 
-# 1) Petal.Length > 1.9
-#   3) Petal.Width <= 1.7; criterion = 1, statistic = 48.825
-#     4) Petal.Length <= 4.8; criterion = 0.999, statistic = 12.872
-#       5)*  weights = 37 
-#     4) Petal.Length > 4.8
-#       6)*  weights = 7 
-#   3) Petal.Width > 1.7
-#     7)*  weights = 31 
-
 plot(iris_ctree)
-
 plot(iris_ctree, type="simple")
 
-testPred <- predict(iris_ctree, newdata=testData)
+# Prediction & Evaluation
+partypred <- predict(iris_ctree, testData)
+confusionMatrix(partypred, testData$Species) # 0.8889
 
-table(testPred, testData$Species)
-# testPred     setosa versicolor virginica
-# setosa         17          0         0
-# versicolor      0          9         0
-# virginica       0          1        15
-
-
-
+# party 패키지는 prune 작업 필요 없으나 입력변수 제한 있음
